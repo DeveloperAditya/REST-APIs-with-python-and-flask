8647804987c365318329237bde336d0accfe2e11
Aditya Patel <52347812+DeveloperAditya@users.noreply.github.com>
2020-05-21 18:21:05 +0530

    creating the application endpoints

--- Section_03_First_REST_API/app.py
+++ Section_03_First_REST_API/app.py
@@ -2,10 +2,53 @@ from flask import Flask
 
 app = Flask(__name__)
 
+stores = [
+    {
+        'name': 'My wonderful store',
+        'items': [
+            {
+                'name': 'My item',
+                'price': 15.99
+            }
+        ]
+    }
+]
 
+"""
+POST - used to receive data
+GET - used to send data back only
+"""
+
+# home page
 @app.route('/')
 def home():
     return 'Hello World!'
+
+
+# POST /store data: { name: }
+@app.route('store', methods=['POST'])
+def create_store():
+    pass
+
+# GET /store/<string:name>
+@app.route('/store/<string:name>')  # http://localhost:5000/store/some_name
+def get_store(name):
+    pass
+
+# GET /store
+@app.route('/store')
+def get_stores():
+    pass
+
+# POST /store/<string:name>/item { name: , price: }
+@app.route('/store/<string:name>/item', methods=['POST'])
+def create_item_in_store(name):
+    pass
+
+# GET /store/<string:name>/item
+@app.route('/store/<string:name>/item')
+def get_item_in_store(name):
+    pass
 
 
 app.run(port=5000)
