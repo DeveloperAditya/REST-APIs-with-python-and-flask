698821111d5dee459d5d1e889eac44959c776f90
Aditya Patel <52347812+DeveloperAditya@users.noreply.github.com>
2020-05-22 14:42:37 +0530

    implementating other endpoints

--- Section_03_First_REST_API/app.py
+++ Section_03_First_REST_API/app.py
@@ -39,7 +39,15 @@ def create_store():
 # GET /store/<string:name>
 @app.route('/store/<string:name>')  # http://localhost:5000/store/some_name
 def get_store(name):
-    pass
+    """
+    iterate over stores,
+    if the store matches, return it &
+    if none match, return error message
+    """
+    for store in stores:
+        if store['name'] == name:
+            return jsonify(store)
+    return jsonify({'message': 'Store not found!'})
 
 # GET /store
 @app.route('/store')
@@ -49,12 +57,24 @@ def get_stores():
 # POST /store/<string:name>/item { name: , price: }
 @app.route('/store/<string:name>/item', methods=['POST'])
 def create_item_in_store(name):
-    pass
+    request_data = request.get_json()
+    for store in stores:
+        if store['name'] == name:
+            new_item = {
+                'name': request_data['name'],
+                'price': request_data['price']
+            }
+            store['items'].append(new_item)
+            return jsonify(new_item)
+    return jsonify({'message': 'Store not found!'})
 
 # GET /store/<string:name>/item
 @app.route('/store/<string:name>/item')
 def get_item_in_store(name):
-    pass
+    for store in stores:
+        if store['name'] == name:
+            return jsonify({'items': store['items']})
+    return jsonify({'message': 'Store not found!'})
 
 
 app.run(port=5000)
