af0ae1a4ff76af9451e24525808ce476c809f8ea
Aditya Patel <52347812+DeveloperAditya@users.noreply.github.com>
2020-05-22 16:46:14 +0530

    improving the code and error control

--- Section_04_Flask_RESTful/code/app.py
+++ Section_04_Flask_RESTful/code/app.py
@@ -9,12 +9,13 @@ items = []
 
 class Item(Resource):
     def get(self, name):
-        for item in items:
-            if item['name'] == name:
-                return item
-        return {'item': None}, 404
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        return {'item': None}, 200 if item is not None else 404
 
     def post(self, name):
+        if next(filter(lambda x: x['name'] == name, items), None):
+            return {'message': 'An item with name {} is already exists'.format(name)}, 400
+
         data = request.get_json()
         item = {
             'name': name,
