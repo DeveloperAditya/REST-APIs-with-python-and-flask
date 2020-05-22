24fa543cb814115391df7a02046c27fa93227e59
Aditya Patel <52347812+DeveloperAditya@users.noreply.github.com>
2020-05-22 17:28:38 +0530

    Authentication and logging in - part-2

--- Section_04_Flask_RESTful/code/app.py
+++ Section_04_Flask_RESTful/code/app.py
@@ -1,17 +1,22 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+from security import authenticate, identity
 
 app = Flask(__name__)
 app.secret_key = 'Aditya'
 api = Api(app)
 
+jwt = JWT(app, authenticate, identity)  # /auth
+
 items = []
 
 
 class Item(Resource):
+    @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x['name'] == name, items), None)
-        return {'item': None}, 200 if item is not None else 404
+        return {'item': item}, 200 if item is not None else 404
 
     def post(self, name):
         if next(filter(lambda x: x['name'] == name, items), None):
